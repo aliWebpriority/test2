e47432a66c6ab48dbf78515f91ea554b98ee3f9a
at1 <alizaidi@webpriority.net>
2016-09-07 23:20:18 -0700

    t1

--- WebApplication4/WebForm2.aspx.cs
+++ WebApplication4/WebForm2.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication4
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            TextBox1.Text = "hellow12";
+
         }
     }
 }
